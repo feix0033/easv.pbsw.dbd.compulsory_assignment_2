3b42c5a83f491cb4f4287386be69451e2d6e16c8
feix0033 <evensnachi1@gmail.com>
2025-05-12 08:28:20 +0200

    connect mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,25 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { DataSource } from 'typeorm';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
+import { CatsModule } from './cats/cats.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: '.env'
+      envFilePath: '.env',
     }),
     DatabaseModule,
-    UsersModule
+    UsersModule,
+    CatsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {
   /* DI the datasource */
-  constructor(private dataSource: DataSource) { } b
+  constructor(private dataSource: DataSource) {}
+  b;
 }
--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,24 +1,26 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-	imports: [
-		TypeOrmModule.forRootAsync({
-			imports: [ConfigModule],
-			inject: [ConfigService],
-			useFactory: (configService: ConfigService) => ({
-				type: 'postgres',
-				host: configService.get('DB_HOST'),
-				port: configService.get('DB_PORT'),
-				username: configService.get('DB_USERNAME'),
-				password: configService.get('DB_PASSWORD'),
-				database: configService.get('DB_DATABASE'),
-				entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-				synchronize: true, // 生产环境建议关闭
-				logging: true
-			})
-		})
-	]
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE'),
+        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+        synchronize: true, // 生产环境建议关闭
+        logging: true,
+      }),
+    }),
+    MongooseModule.forRoot('mongodb://root:example@mongo:27017/test?authSource=admin'),
+  ],
 })
-export class DatabaseModule { }
+export class DatabaseModule {}
